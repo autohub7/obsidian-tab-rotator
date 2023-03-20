4a56cd44e1b18c24124dbf62c3414f7a98b0a9b2
Steven Jin <vlsijin@gmail.com>
2023-03-20 09:36:17 -0700

    Init

--- main.ts
+++ main.ts
@@ -1,85 +1,156 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Settings {
+	interval: string;
+	enabled: boolean;
+	viewTypes: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: Settings = {
+	interval: '5',
+	enabled: true,
+	viewTypes: ['markdown', 'canvas'],
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: Settings;
+	private timerId: number;
+	private enabled: boolean = false;
+	private direction: boolean = true;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon('rotate-cw', 'Tab Rotation', (evt: MouseEvent) => {
+			this.enabled = !this.enabled
+			if (this.enabled) {
+				this.direction = true;
+				this.startTabRotation();
+				new Notice('Tap right rotation enabled!');
+			} else {
+				this.stopTabRotation();
+				new Notice('Tap rotation disabled!');
+			}
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'rotate-right',
+			name: 'Start rotating to the right',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if(this.enabled && !this.direction) {
+					this.direction = false;
+					this.stopTabRotation();
+					this.startTabRotation();
+					new Notice('Tap right rotation enabled!');
 				}
-			}
+				else if(!this.enabled) {
+					this.direction = true;
+					this.startTabRotation();
+					this.enabled = !this.enabled
+					new Notice('Tap right rotation enabled!');
+				}
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'rotate-left',
+			name: 'Start rotating to the left',
+			callback: () => {
+				if(this.enabled && !this.direction) {
+					this.direction = true;
+					this.stopTabRotation();
+					this.startTabRotation();
+					new Notice('Tap left rotation enabled!');
+				}
+				else if(!this.enabled) {
+					this.direction = false;
+					this.startTabRotation()
+					this.enabled = !this.enabled
+					new Notice('Tap left rotation enabled!');
+				}
+			},
+		});	  
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: 'stop-rotation',
+			name: 'Stop tab rotation',
+			callback: () => {
+				if(this.enabled) {
+					this.stopTabRotation();
+					this.enabled = !this.enabled
+					new Notice('Tap rotation disabled!');
+				}
+			},
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	startTabRotation() {
+		if (!isNumeric(this.settings.interval)) {
+			new Notice('The specified internval is not number. Rotate with the default interval 5 secs');
+		}
 
+    	this.timerId = window.setInterval(() => {
+			(this.direction)? this.rotateRightTabs(): this.rotateLeftTabs()
+    	}, parseInt(this.settings.interval) * 1000);
+	}
+
+	stopTabRotation() {
+		window.clearInterval(this.timerId);
+	}
+
+	getLeavesOfTypes(types: string[]): WorkspaceLeaf[] {
+		const leaves: WorkspaceLeaf[] = [];
+
+		this.app.workspace.iterateAllLeaves((leaf) => {
+			const isMainWindow = leaf.view.containerEl.win == window;
+			const correctViewType = types.contains(leaf.view.getViewType());
+			const sameWindow = leaf.view.containerEl.win == activeWindow;
+
+			//Ignore sidebar panes in the main window, because non-main window don't have a sidebar
+			const correctPane = isMainWindow ? (sameWindow && leaf.getRoot() == this.app.workspace.rootSplit) : sameWindow;
+			if (
+				correctViewType
+				&& correctPane
+			) {
+				leaves.push(leaf);
+			}
+		});
+
+		return leaves;
+	}
+
+	async rotateRightTabs() {
+		const active = this.app.workspace.getLeaf();
+		const leaves: WorkspaceLeaf[] = this.getLeavesOfTypes(this.settings.viewTypes);
+		
+		if (active) {
+			const index = leaves.indexOf(active);
+			if (index === leaves.length - 1) {
+				this.app.workspace.setActiveLeaf(leaves[0], {focus: true});
+			} else {
+				this.app.workspace.setActiveLeaf(leaves[index + 1], {focus: true});
+			}
+		}
+	}
+
+	async rotateLeftTabs() {
+		const active = this.app.workspace.getLeaf();
+		const leaves: WorkspaceLeaf[] = this.getLeavesOfTypes(this.settings.viewTypes);
+		
+		if (active) {
+			const index = leaves.indexOf(active);
+			if (index === 0) {
+				this.app.workspace.setActiveLeaf(leaves[leaves.length - 1], {focus: true});
+			} else {
+				this.app.workspace.setActiveLeaf(leaves[index - 1], {focus: true});
+			}
+		}
+	}
+
+	async onunload() {
+		window.clearInterval(this.timerId);
 	}
 
 	async loadSettings() {
@@ -91,23 +162,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -117,21 +172,26 @@ class SampleSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
+		
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Tab Rotation PlugIn'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Interval')
+			.setDesc('Set Interval (secs)')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setPlaceholder('Enter interval (secs)')
+			.setValue(this.plugin.settings.interval)
+			.onChange(async (value) => {
+				this.plugin.settings.interval = value;
+				await this.plugin.saveSettings();
+			}));
 	}
+}
+
+function isNumeric(str: string) {
+	if (typeof str != "string") {
+	  return false;
+	}
+	return !isNaN(Number(str)) && !isNaN(parseFloat(str));
 }
